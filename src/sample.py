01302ed079c381c955e16b6dc23117c64e9d20fc
rokkish <rokki.hobby@gmail.com>
2024-03-10 04:35:14 +0900

    Add quiz hint initialization

--- src/sample.py
+++ src/sample.py
@@ -197,6 +197,7 @@ async def quiz_morgana_genre(interaction: discord.Interaction, genre: QuizGenres
         await send_msg.edit(content=f"{p_bar.print('ヒント生成 done...')}")
 
         quiz.summary = quiz.get_summary(quiz.title)
+        quiz.init_hint()
         quiz.images = quiz.get_images(quiz.title)
         await send_msg.edit(content=f"{p_bar.print('大ヒント生成 done...')}")
 
